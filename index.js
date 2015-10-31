c535d42e3dc87ce442f4d2defbc607ee59c3eb20
Tim Diggins <tim@red56.co.uk>
2015-10-31 14:40:50 +0000

    more implementation

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var Permatime = function (root, zone, dateString, timeString, timestampZone, timestamp) {
-  this.root = root || "/";
-  this.zone = zone || timestampZone;
-  this.dateString = dateString || null;
-  this.timeString = timeString || null;
-  this.timestamp = timestamp || null;
+var Permatime = function (parts) {
+  //root, zone, date, time, timestamp
+  this.root = parts.root || "/";
+  this.zone = parts.zone || null;
+  this.date = parts.date || null;
+  this.time = parts.time || null;
+  this.timestamp = parts.timestamp || null;
+  this.label = parts.label || null;
+  this.link = parts.link || null;
 };
 
-var rootMatcher = "(https?://[^/]+/|/)?";
-var zoneMatcher = "(.+)";
-var dateTimeMatcher = "/(\\d{4}-\\d{2}-\\d{2})/(\\d{2}:\\d{2})";
-var timestampMatcher = "(timestamp)/(\\d+)()";
-var matcher = new RegExp(rootMatcher + "(?:" + zoneMatcher + dateTimeMatcher + "|" + timestampMatcher + ")");
+var root = "^(https?://[^/]+/|/)?";
+var zone = "((?:[^/]+/)*[^/]+)";
+var dateTime = "/(\\d{4}-\\d{2}-\\d{2})/(\\d{2}:\\d{2})";
+var timestamp = "(timestamp)/(\\d+)()";
+var label = "(?:/([^/?]+))?";
+var link = "(?:.*[?&]link=([^&]+))?";
+var zMatcher = new RegExp(root + zone + dateTime + label + link);
+var tsMatcher = new RegExp(root + timestamp + label + link);
 
 var parseFunction = function (permatimeString) {
-  var parts = matcher.exec(permatimeString);
-  return new Permatime(parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
+  var parts = zMatcher.exec(permatimeString);
+  if(parts) {
+    return new Permatime({root: parts[1], zone: parts[2], date: parts[3], time: parts[4], label: parts[5], link: parts[6]});
+  }
+  parts = tsMatcher.exec(permatimeString);
+  if (parts) {
+    return new Permatime({root: parts[1], timestamp: parts[3], label: parts[5], link: parts[6]});
+  }
+  return null;
 };
 
-module.exports = parseFunction;
+module.exports = {parse: parseFunction, Permatime: Permatime};
--- test/parser_test.js
+++ test/parser_test.js
@@ -1,12 +1,13 @@
 var test = require('tape');
-var parse = require('../index.js');
+var permatime = require('../index.js');
+var parse = permatime.parse;
 
 test('minimal datetime with host', function (t) {
   var permatime = parse("http://permatime.com/Europe/Berlin/1989-11-09/00:01");
   t.equal(permatime.root, "http://permatime.com/");
   t.equal(permatime.zone, "Europe/Berlin");
-  t.equal(permatime.dateString, "1989-11-09");
-  t.equal(permatime.timeString, "00:01");
+  t.equal(permatime.date, "1989-11-09");
+  t.equal(permatime.time, "00:01");
   t.end();
 });
 
@@ -14,8 +15,8 @@ test('minimal datetime without host', function (t) {
   var permatime = parse("/Europe/Berlin/1989-11-09/00:01");
   t.equal(permatime.root, "/");
   t.equal(permatime.zone, "Europe/Berlin");
-  t.equal(permatime.dateString, "1989-11-09");
-  t.equal(permatime.timeString, "00:01");
+  t.equal(permatime.date, "1989-11-09");
+  t.equal(permatime.time, "00:01");
   t.equal(permatime.timestamp, null);
   t.end();
 });
@@ -24,26 +25,64 @@ test('minimal datetime with bare root', function (t) {
   var permatime = parse("Europe/Berlin/1989-11-09/00:01");
   t.equal(permatime.root, "/");
   t.equal(permatime.zone, "Europe/Berlin");
-  t.equal(permatime.dateString, "1989-11-09");
-  t.equal(permatime.timeString, "00:01");
-  t.equal(permatime.timestamp, null);
   t.end();
 });
 
 test('minimal timestamp', function (t) {
   var permatime = parse("timestamp/1226183760");
   t.equal(permatime.root, "/");
-  t.equal(permatime.zone, "timestamp");
-  t.equal(permatime.dateString, null);
-  t.equal(permatime.timeString, null);
+  t.equal(permatime.zone, null);
+  t.equal(permatime.date, null);
+  t.equal(permatime.time, null);
   t.equal(permatime.timestamp, "1226183760");
   t.end();
 });
 
-test.skip("finds optional label");
-test.skip("finds optional link");
-test.skip("finds optional label and optional link");
+test("finds optional label", function (t) {
+  var permatime = parse("/Europe/Berlin/1989-11-09/00:01/Berlin_Wall");
+  t.equal(permatime.zone, "Europe/Berlin");
+  t.equal(permatime.label, "Berlin_Wall");
+  t.equal(permatime.link, null);
+  t.end();
+});
+test("finds optional link", function (t) {
+  var permatime = parse("/Europe/Berlin/1989-11-09/00:01?link=http://myblog.com");
+  t.equal(permatime.zone, "Europe/Berlin");
+  t.equal(permatime.label, null);
+  t.equal(permatime.link, "http://myblog.com");
+  t.end();
+});
+test("maximal datetime", function (t) {
+  var permatime = parse("/Europe/Berlin/1989-11-09/00:01/Berlin_Wall?link=http://myblog.com");
+  t.equal(permatime.zone, "Europe/Berlin");
+  t.equal(permatime.label, "Berlin_Wall");
+  t.equal(permatime.link, "http://myblog.com");
+  t.end();
+});
+test("maximal timestamp", function (t) {
+  var permatime = parse("/timestamp/1226183760/Berlin_Wall?link=http://myblog.com");
+  t.equal(permatime.timestamp, "1226183760");
+  t.equal(permatime.label, "Berlin_Wall");
+  t.equal(permatime.link, "http://myblog.com");
+  t.end();
+});
 
-test.skip("finds optional link even with other unsupported parameters");
+test("finds optional link even with other unsupported parameters", function (t) {
+  var permatime = parse("/Europe/Berlin/1989-11-09/00:01/Berlin_Wall?some=other&link=http://myblog.com&else=rubbish");
+  t.equal(permatime.zone, "Europe/Berlin");
+  t.equal(permatime.label, "Berlin_Wall");
+  t.equal(permatime.link, "http://myblog.com");
+  t.end();
+});
 
-test.skip("won't match with rubbish before root");
+test("erroneous permatime", function(t){
+  var permatime = parse("/Europe/Berlin/1989-11-09");
+  t.equal(permatime, null);
+  t.end();
+});
+
+test("won't match with rubbish before root", function(t){
+  var permatime = parse("rubbishhttp://permatime.com/Europe/Berlin/1989-11-09/00:01");
+  t.equal(permatime, null);
+  t.end();
+});
